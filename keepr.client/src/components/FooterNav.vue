aa987d58d03e841a63513724918f9f06a620b861
TungLe0319 <tung.le0319@gmail.com>
2022-12-08 15:48:48 -0700

    tung

--- keepr.client/src/components/FooterNav.vue
+++ keepr.client/src/components/FooterNav.vue
@@ -59,14 +59,14 @@
         ></i> -->
         <Login class="d-md-none" />
       </div>
-      <img
+      <!-- <img
         src="../assets/img/Keepr logo.png"
         alt="keepr Logo"
         width="70"
         height="60"
         class="d-none d-md-block"
         title="keepr Logo"
-      />
+      /> -->
       <Login class="d-none d-md-block" />
     </div>
   </nav>
