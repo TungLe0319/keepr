e970db0c0389e3dc1a956aabb475f639dd27a686
TungLe0319 <tung.le0319@gmail.com>
2022-12-09 13:58:57 -0700

    tung

--- keepr.client/src/env.js
+++ keepr.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost');
-export const baseURL = dev ? 'https://localhost:7045' : '';
+export const baseURL = dev ? 'https://keepr-project-demo.onrender.com' : "";
 export const useSockets = false;
 export const domain = 'bcwtungle.us.auth0.com';
 export const audience = 'https://bcw-cocobeans.com';
